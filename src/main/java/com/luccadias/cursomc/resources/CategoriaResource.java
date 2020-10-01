daab8693cc56fd88809d12421c75976393edc103
Lucca Dias <lucca.gdias07@gmail.com>
2020-10-01 11:01:06 -0300

    Listando todas as Categorias com DTO

--- src/main/java/com/luccadias/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/luccadias/cursomc/resources/CategoriaResource.java
@@ -1,10 +1,13 @@
 package com.luccadias.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.luccadias.cursomc.domain.Categoria;
+import com.luccadias.cursomc.dto.CategoriaDTO;
 import com.luccadias.cursomc.services.CategoriaService;
 
 @RestController
@@ -57,5 +61,14 @@ public class CategoriaResource {
 		service.delete(id);
 		
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(listDTO);
 	}
 }
--- src/main/java/com/luccadias/cursomc/services/CategoriaService.java
+++ src/main/java/com/luccadias/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.luccadias.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -46,5 +47,9 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possível exluir uma categoria que possui produtos!");
 		}
 		
+	}
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 }
