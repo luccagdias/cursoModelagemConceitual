451876d6e75d95f3fffb2d941966e9fcefe09bf9
Lucca Dias <lucca.gdias07@gmail.com>
2020-10-01 02:09:47 -0300

    Endpoint /pedidos/{id} disponibilizado e Atualização: utilizando somente JsonIgnore

--- src/main/java/com/luccadias/cursomc/domain/Pedido.java
+++ src/main/java/com/luccadias/cursomc/domain/Pedido.java
@@ -15,7 +15,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 public class Pedido implements Serializable {
@@ -25,12 +25,13 @@ public class Pedido implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+
+	@JsonFormat(pattern = "dd/MM/yyyy HH:mm")
 	private Date instante;
 
 	@OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido")
 	private Pagamento pagamento;
 
-	@JsonManagedReference
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private Cliente cliente;
