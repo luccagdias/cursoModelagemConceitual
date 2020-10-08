180b7e0179a09167ae96bd647ef44ff9843d2942
Lucca Dias <lucca.gdias07@gmail.com>
2020-10-08 19:59:35 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/luccadias/cursomc/services/PedidoService.java
+++ src/main/java/com/luccadias/cursomc/services/PedidoService.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.luccadias.cursomc.domain.Categoria;
 import com.luccadias.cursomc.domain.Pedido;
 import com.luccadias.cursomc.repositories.PedidoRepository;
 import com.luccadias.cursomc.services.exceptions.ObjectNotFoundException;
@@ -14,12 +13,12 @@ import com.luccadias.cursomc.services.exceptions.ObjectNotFoundException;
 public class PedidoService {
 
 	@Autowired
-	PedidoRepository pedidoRepository;
+	PedidoRepository repo;
 	
 	public Pedido find(Integer id) {
-		Optional<Pedido> obj = pedidoRepository.findById(id);
+		Optional<Pedido> obj = repo.findById(id);
 		
 		return obj.orElseThrow(() -> 
-			new ObjectNotFoundException("Objeto não encontrado! Id: " + id + " Tipo: " + Categoria.class.getName()));
+			new ObjectNotFoundException("Objeto não encontrado! Id: " + id + " Tipo: " + Pedido.class.getName()));
 	}
 }
---
+++ src/main/java/com/luccadias/cursomc/services/ProdutoService.java
@@ -0,0 +1,42 @@
+package com.luccadias.cursomc.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import com.luccadias.cursomc.domain.Categoria;
+import com.luccadias.cursomc.domain.Produto;
+import com.luccadias.cursomc.repositories.CategoriaRepository;
+import com.luccadias.cursomc.repositories.ProdutoRepository;
+import com.luccadias.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ProdutoService {
+
+	@Autowired
+	ProdutoRepository repo;
+	
+	@Autowired
+	CategoriaRepository categoriaRepository;
+	
+	public Produto find(Integer id) {
+		Optional<Produto> obj = repo.findById(id);
+		
+		return obj.orElseThrow(() -> 
+			new ObjectNotFoundException("Objeto não encontrado! Id: " + id + " Tipo: " + Produto.class.getName()));
+	}
+	
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest =  PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		List<Categoria> categorias = categoriaRepository.findAllById(ids);
+		
+		return repo.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);
+		
+	}
+}
