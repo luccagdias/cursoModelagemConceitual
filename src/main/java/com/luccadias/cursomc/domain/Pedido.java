1fdb04d284131181e898f72d871c32c0355011ca
Lucca Dias <lucca.gdias07@gmail.com>
2020-10-18 17:03:09 -0300

    Subtotal e total do Pedido

--- src/main/java/com/luccadias/cursomc/domain/Pedido.java
+++ src/main/java/com/luccadias/cursomc/domain/Pedido.java
@@ -54,6 +54,16 @@ public class Pedido implements Serializable {
 		this.cliente = cliente;
 		this.enderecoEntrega = enderecoEntrega;
 	}
+	
+	public double getValorTotal() {
+		double soma = 0.0;
+		
+		for (ItemPedido ip : itens) {
+			soma += ip.getSubTotal();
+		}
+		
+		return soma;
+	}
 
 	public Integer getId() {
 		return id;
